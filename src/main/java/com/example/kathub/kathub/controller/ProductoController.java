99cddc62056389aaf198f21c64f455540a55f6a2
fernanditaa <163853793+fernanditaa@users.noreply.github.com>
2025-11-24 20:21:07 -0300

    dockers

--- src/main/java/com/example/kathub/kathub/controller/ProductoController.java
+++ src/main/java/com/example/kathub/kathub/controller/ProductoController.java
@@ -3,19 +3,20 @@ package com.example.kathub.kathub.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.http.ResponseEntity;
+
 import com.example.kathub.kathub.model.Producto;
 import com.example.kathub.kathub.service.ProductoService;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.DeleteMapping;
 
 
 @RestController
